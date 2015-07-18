551aef0b39ec7f345d014ad93e2d7a740a24aa5f
Esoft <Esoft@A1-2-8.esoft.lan>
2015-07-18 12:19:40 +0530

    Commit Project to Local Repository-chalaka

--- src/java/com/dao/LoginDAOImpl.java
+++ src/java/com/dao/LoginDAOImpl.java
@@ -18,27 +18,57 @@ import java.util.logging.Logger;
  */
 public class LoginDAOImpl {
 
+    
+    //get the loging info that user provide and validate it
     public String login(String username, String password) {
+        
+        //make DataBase connection object 
         Connection conn = DBConnection.getConnection();
+        
+        //make sure connection is work correctly by using connection object
+        //if connection is valid
         if (conn != null) {
+            
+            //make  a query to  execute and check that user provide data are valid 
+            //and put that out put result in to string variable
             String loginQuery = "SELECT * FROM login WHERE username=? AND password=?";
 
+            
             try {
+                //make a object to handle that query result variable
                 PreparedStatement pstmt = conn.prepareStatement(loginQuery);
+                
+                //put the checking values that user provide  to validate by using were condition
                 pstmt.setString(1, username);
                 pstmt.setString(2, password);
+                
+                //execute the query to get result
                 ResultSet rs = pstmt.executeQuery();
+                
+                //if result is valid 
                 if (rs.next()) {
+                    
+                    //get usertype of that correct result and put it into the userType variable
                     String userType = rs.getString(2);
 
+                    //return out put as "success" 
                     return "success-" + userType;
+                    
+                 //if result is invalid 
                 } else {
+                    //return out put as "invalid" 
                     return "invalid";
                 }
+                
+                
+                //if catching some errors
             } catch (Exception ex) {
+                //print that errors
                 ex.printStackTrace();
             }
         }
+        
+        //if connecton is not valid
         return null;
     }
 
