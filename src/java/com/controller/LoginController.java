cb31184f09117f1a28847c67eb4f73a7e51797ab
Esoft <Esoft@A1-2-10.esoft.lan>
2015-07-18 11:33:50 +0530

    Merge origin/master
    Conflicts:
    	src/java/controlller/LoginController.java
    
    add servlet controller

--- src/java/com/controller/LoginController.java
+++ src/java/com/controller/LoginController.java
@@ -7,6 +7,7 @@ package com.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,16 +33,23 @@ public class LoginController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
+            
+             String loginame=request.getParameter("loginame");
+            String password=request.getParameter("passwrod");
+            
+            String user=getServletConfig().getInitParameter(loginame);
+            String Password=getServletConfig().getInitParameter(user);
+            
+            if(user.equals(loginame) && Password.equals(password))
+            {
+                  response.sendRedirect("Welcome.jsp");
+            }
+              else
+            {
+                RequestDispatcher rd= getServletContext().getRequestDispatcher("/index.jsp");
+            }
+            
         } finally {
             out.close();
         }
